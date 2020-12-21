49e5bce739fff6657a091ff37afc884b9f37858f
ensar.temel <ensar.temel@kodfu.com>
2020-12-21 11:35:39 +0300

    Update results.php

--- templates/coffeever/components/results.php
+++ templates/coffeever/components/results.php
@@ -1,7 +1,6 @@
 <?php
 defined('INDEX') or die();
-debugPrint($_GET);
-
+debugPrint($pageData);
 ?>
 <section class="home-slider owl-carousel">
 <div class="slider-item" style="background-image: url(templates/coffeever/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
