6112829aa778c20cb09093a45600a922a65b64eb
ensar.temel <ensar.temel@kodfu.com>
2020-12-21 11:42:41 +0300

    Update results.php

--- templates/coffeever/components/results.php
+++ templates/coffeever/components/results.php
@@ -1,6 +1,5 @@
 <?php
 defined('INDEX') or die();
-debugPrint($pageData);
 ?>
 <section class="home-slider owl-carousel">
 <div class="slider-item" style="background-image: url(templates/coffeever/images/bg_3.jpg);" data-stellar-background-ratio="0.5">
@@ -27,7 +26,6 @@ debugPrint($pageData);
                         <tr class="text-center">
                           <th>&nbsp;</th>
                           <th>Coffee</th>
-                          <th>Relevancy to your taste</th>
                           <th>Aroma</th>
                           <th>Acidity</th>
                           <th>Body</th>
@@ -39,100 +37,26 @@ debugPrint($pageData);
                       </thead>
                       <tbody>
                         <tr class="text-center">
-                         
+                          <?php foreach($pageData as $result): ?>
                           <td class="image-prod"><div class="img" style="background-image:url(templates/coffeever/images/menu-2.jpg);"></div></td>
                           
                           <td class="product-name">
-                              <h3>Americano</h3>
-                              <p>Far far away, behind the word mountains, far from the countries</p>
+                              <h3><?php echo $result['name'] ?></h3>
+                              <p><?php echo $result['blindAssesment'] ?></p>
                           </td>
                           
-                          <td class="price">84%</td>
-                          
-                          <td class="total" id="aroma">7</td>                                
-                          <td class="total" id="acidity">9</td>
-                          <td class="total" id="body">6</td>
-                          <td class="total" id="flavor">8</td>
-                          <td class="total" id="decaf">No</td>
-                          <td class="total" id="roast">Medium</td>
+                          <td class="total" id="aroma"><?php echo $result['aroma'] ?></td>                                
+                          <td class="total" id="acidity"><?php echo $result['acidity'] ?></td>
+                          <td class="total" id="body"><?php echo $result['body'] ?></td>
+                          <td class="total" id="flavor"><?php echo $result['flavor'] ?></td>
+                          <td class="total" id="decaf"><?php echo ($result['decaf']) ? 'Yes' : 'No'; ?></td>
+                          <td class="total" id="roast"><?php echo $result['roast'] ?></td>
 
                           <td  class="favorite">
                               <button type="button" id="button1" class="favoritebutton" onclick="Warn(this.id);">Favorite
                               </button>
                           </td>
-
-                        </tr>
-
-                        <tr class="text-center">
-                          
-                          <td class="image-prod"><div class="img" style="background-image:url(templates/coffeever/images/menu-2.jpg);"></div></td>
-                          
-                          <td class="product-name">
-                              <h3>Cortado</h3>
-                              <p>Far far away, behind the word mountains, far from the countries</p>
-                          </td>
-                          
-                          <td class="price">78%</td>
-                          
-                          <td class="total" id="aroma">7</td>                                
-                          <td class="total" id="acidity">8</td>
-                          <td class="total" id="body">7</td>
-                          <td class="total" id="flavor">8</td>
-                          <td class="total" id="decaf">No</td>
-                          <td class="total" id="roast">Medium</td>
-
-                          <td  class="favorite">
-                              <button type="button" id="button2" class="favoritebutton" onclick="Warn(this.id);">Favorite
-                              </button>
-                          </td>
-                          
-                        </tr>
-                         <tr class="text-center">
-                          
-                          <td class="image-prod"><div class="img" style="background-image:url(templates/coffeever/images/menu-2.jpg);"></div>
-                          </td>
-                          
-                          <td class="product-name">
-                              <h3>Latte</h3>
-                              <p>Far far away, behind the word mountains, far from the countries</p>
-                          </td>
-                          
-                          <td class="price">75%</td>
-                          
-                          <td class="total" id="aroma">7</td>                                
-                          <td class="total" id="acidity">6</td>
-                          <td class="total" id="body">6</td>
-                          <td class="total" id="flavor">6</td>
-                          <td class="total" id="decaf">No</td>
-                          <td class="total" id="roast">Medium</td>
-
-                          <td  class="favorite">
-                              <button type="button" id="button3" class="favoritebutton" onclick="Warn(this.id);">Favorite
-                              </button></td>
-                          
-                        </tr>
-                         <tr class="text-center">
-                          
-                          <td class="image-prod"><div class="img" style="background-image:url(templates/coffeever/images/menu-2.jpg);"></div></td>
-                          
-                          <td class="product-name">
-                              <h3>Cappucino</h3>
-                              <p>Far far away, behind the word mountains, far from the countries</p>
-                          </td>
-                          
-                          <td class="price">718%</td>
-                          
-                          <td class="total" id="aroma">6</td>                                
-                          <td class="total" id="acidity">6</td>
-                          <td class="total" id="body">5</td>
-                          <td class="total" id="flavor">5</td>
-                          <td class="total" id="decaf">No</td>
-                          <td class="total" id="roast">Medium_Light</td>
-
-                          <td  class="favorite">
-                              <button type="button" id="button4" class="favoritebutton" onclick="Warn(this.id);">Favorite
-                              </button></td>
-                          
+                          <?php endforeach; ?>
                         </tr>
                       </tbody>
                     </table>
