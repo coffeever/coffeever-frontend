55bf74bc4578695046917f4fb1ee50063540b591
ensar.temel <ensar.temel@kodfu.com>
2021-01-16 20:02:16 +0300

    Update coffees.php

--- templates/coffeever/components/coffees.php
+++ templates/coffeever/components/coffees.php
@@ -8,7 +8,7 @@ $filters = [
   "offset" => getSkipSize()
 ];
 $productsRequest = makeRequest("getSomeCoffee", $filters, 'GET');
-debugPrint($filters);
+debugPrint($productsRequest);
 $newCoffees = isset($productsRequest["result"]) ? $productsRequest["result"] : [];
 $total = !empty($coffees) ? count($coffees) : 0;
 $pageMax = !empty($total) ? calcPageMax($total) : 1;
