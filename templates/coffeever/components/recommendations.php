679c49a82bd727d9ea0ebdf7eddcf040cfb6c45c
ensar.temel <ensar.temel@kodfu.com>
2021-01-16 22:55:52 +0300

    Update recommendations.php

--- templates/coffeever/components/recommendations.php
+++ templates/coffeever/components/recommendations.php
@@ -40,6 +40,14 @@ $user = makeRequest("login", $_SESSION['subscriberObj'], 'POST');
                       <tbody>
                             <?php if($user['favorites'] != ""):?>
                                 <?php foreach($favs as $result): ?>
+                                <?php 
+                                $isFav = false;
+                                if(isLoggedIn()){
+                                  if(str_contains($_SESSION['subscriberObj']['favorites'], $result['slug'])) {
+                                    $isFav = true;
+                                  }
+                                }
+                                ?>
                                 <tr class="text-center">
                                     <td class="image-prod"><div class="img" style="background-image:url(templates/coffeever/images/menu-2.jpg);"></div></td>
                                     
@@ -54,15 +62,27 @@ $user = makeRequest("login", $_SESSION['subscriberObj'], 'POST');
                                     <td class="total" id="flavor"><?php echo $result['flavor'] ?></td>
                                     <td class="total" id="decaf"><?php echo ($result['decaf']) ? 'Yes' : 'No'; ?></td>
                                     <td class="total" id="roast"><?php echo $result['roast'] ?></td>
-                                    <td  class="favorite">
+                                    <?php if($isFav): ?>
+                                      <td  class="favorite">
                                         <form method="post">
                                             <input type="hidden" name="_nonce" value="<?php echo md5(INDEX); ?>">
-                                            <input type="hidden" name="action" value="delete-fav">
+                                            <input type="hidden" name="action" value="add-fav">
                                             <input type="hidden" name="user-id" value="<?php echo $_SESSION['subscriberObj']['google_id'] ?>">
                                             <input type="hidden" name="coffee-slug" value="<?php echo $result['slug'] ?>">
-                                            <button type="submit" class="main-button delete-btn">Delete</button>
+                                            <button type="submit" class="main-button delete-btn">Delete Favourite</button>
                                         </form>
                                     </td>
+                                    <?php else: ?>
+                                      <td  class="favorite">
+                                        <form method="post">
+                                            <input type="hidden" name="_nonce" value="<?php echo md5(INDEX); ?>">
+                                            <input type="hidden" name="action" value="add-fav">
+                                            <input type="hidden" name="user-id" value="<?php echo $_SESSION['subscriberObj']['google_id'] ?>">
+                                            <input type="hidden" name="coffee-slug" value="<?php echo $result['slug'] ?>">
+                                            <button type="submit" class="main-button add-btn">Add Favourite</button>
+                                        </form>
+                                    </td>
+                                    <?php endif; ?>
                                 </tr>
                                 <?php endforeach; ?>
                             <?php else: ?>
