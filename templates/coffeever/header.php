30a7d85e4dd1db938dcb1e55c36a6d03ee49656e
ensar.temel <ensar.temel@kodfu.com>
2021-01-17 00:36:52 +0300

    Update header.php

--- templates/coffeever/header.php
+++ templates/coffeever/header.php
@@ -25,7 +25,7 @@ if (isset($_GET['code'])) {
   $google_account_info = $google_oauth->userinfo->get();
 
   $userObj = [
-    "google_id"=> ((int)$google_account_info->id),
+    "google_id"=> ((float)$google_account_info->id),
     "name"=>  $google_account_info->name,
     "mail"=> $google_account_info->email,
   ];
